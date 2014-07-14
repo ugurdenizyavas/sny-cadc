ca08a9a4355c1b13c888019e5617677e4338f223
trbasarb <baris.basaran@eu.sony.com>
2014-07-14 10:22:41 +0300

    added dev-release.sh

---
+++ dev-release.sh
@@ -0,0 +1,1 @@
+../octopus3-commons/scripts/deploy.sh -i 43.216.132.89 -u GWTSYN_dev_rw -p 9092 -j octopus3-cadc-source-service-1.0-SNAPSHOT-fat.jar -l /opt/logs/cadc-service -e dev -n octopus3-cadc-source-service
