4e1cf4f7e7f27704eb180d504ef1a30811fe0a02
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-07-16 13:24:24 +0300

    Monitoring Services: Healthcheck

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -3,6 +3,7 @@ import com.sony.ebs.octopus3.microservices.cadcsourceservice.handlers.DeltaFlowH
 import com.sony.ebs.octopus3.microservices.cadcsourceservice.handlers.ErrorHandler
 import com.sony.ebs.octopus3.microservices.cadcsourceservice.handlers.SaveFlowHandler
 import com.sony.ebs.octopus3.microservices.cadcsourceservice.handlers.SheetFlowHandler
+import com.sony.ebs.octopus3.microservices.cadcsourceservice.services.MonitoringService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
@@ -12,6 +13,7 @@ import ratpack.jackson.JacksonModule
 import ratpack.rx.RxRatpack
 
 import static ratpack.groovy.Groovy.ratpack
+import static ratpack.jackson.Jackson.json
 
 Logger log = LoggerFactory.getLogger("ratpack");
 
@@ -20,6 +22,7 @@ ratpack {
     SheetFlowHandler sheetFlowHandler
     DeltaFlowHandler deltaFlowHandler
     SaveFlowHandler saveFlowHandler
+    MonitoringService monitoringService
 
     bindings {
         add new JacksonModule()
@@ -33,12 +36,40 @@ ratpack {
             deltaFlowHandler = ctx.getBean(DeltaFlowHandler.class)
             sheetFlowHandler = ctx.getBean(SheetFlowHandler.class)
             saveFlowHandler = ctx.getBean(SaveFlowHandler.class)
+            monitoringService = ctx.getBean MonitoringService.class
 
             RxRatpack.initialize()
         }
     }
 
     handlers {
+
+        get("amazon/healthcheck") {
+            def params = [:]
+
+            params.enabled = request.queryParams.enabled
+
+            if (params.enabled) {
+                def action = params.enabled.toBoolean()
+                if (action) {
+                    monitoringService.up()
+                    response.status(200)
+                    render json(status: 200, message: "App is up for the eyes of LB!")
+                } else {
+                    monitoringService.down()
+                    response.status(200)
+                    render json(status: 200, message: "App is down for the eyes of LB!")
+                }
+            } else {
+                if (monitoringService.checkStatus()) {
+                    response.status(200)
+                    render json(status: 200, message: "Ticking!")
+                } else {
+                    response.status(404)
+                    render json(status: 404, message: "App is down!")
+                }
+            }
+        }
         post("save/repo/:urn", saveFlowHandler)
 
         get("import/sheet/:urn", sheetFlowHandler)
