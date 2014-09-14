d9fe3b85aafe78d54453ab57b604277b52a65b8e
trbasarb <baris.basaran@eu.sony.com>
2014-09-14 19:01:18 +0300

    moved common components to commons-ratpack

--- src/test/groovy/com/sony/ebs/octopus3/microservices/cadcsourceservice/services/DeltaServiceTest.groovy
+++ src/test/groovy/com/sony/ebs/octopus3/microservices/cadcsourceservice/services/DeltaServiceTest.groovy
@@ -20,12 +20,44 @@ import spock.util.concurrent.BlockingVariable
 @Slf4j
 class DeltaServiceTest {
 
+    DeltaService deltaService
+
     @Before
     void before() {
+        deltaService = new DeltaService()
     }
 
     @Test
-    void "success"() {
+    void "test success"() {
+        def response = new MockNingResponse(_statusCode: 200, _responseBody: '{"result" : { "repoUrl" : "http://repo" } }')
+        def result = deltaService.createServiceResult(response, "http://cadc")
+        result.with {
+            assert success == true
+            assert statusCode == 200
+            assert cadcUrl == "http://cadc"
+            assert repoUrl == "http://repo"
+        }
     }
 
+    @Test
+    void "test failure"() {
+        def response = new MockNingResponse(_statusCode: 500, _responseBody: '{"errors" : [ "err1", "err2" ] }')
+        def result = deltaService.createServiceResult(response, "http://cadc")
+        result.with {
+            assert success == false
+            assert statusCode == 500
+            assert cadcUrl == "http://cadc"
+            assert errors == ["err1", "err2"]
+        }
+    }
+
+    @Test
+    void "test error"() {
+        def result = deltaService.createServiceResultOnError("err1", "http://cadc")
+        result.with {
+            assert success == false
+            assert cadcUrl == "http://cadc"
+            assert errors == ["err1"]
+        }
+    }
 }
